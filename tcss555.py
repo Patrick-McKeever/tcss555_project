f900544f50109f46dd70f83441da6f4e3672255b
patrick <patrick.mckeever@protonmail.com>
2024-10-20 14:48:52 -0700

    Add OpenCV face-based CNN (81% gender acc)

--- tcss555.py
+++ tcss555.py
@@ -1,5 +1,3 @@
-# !/usr/bin/python3
-
 import argparse
 import os
 import sys
@@ -14,6 +12,10 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Convolution2D, BatchNormalization, Activation, AveragePooling2D, Dropout, GlobalAveragePooling2D, Dense
 from keras.models import Sequential
 from keras import regularizers, optimizers, models, layers
+
+# Declare this globally to prevent reloading.
+FACE_CASCADE = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+
 
 def check_valid_directory(path):
 	if not os.path.isdir(path):
@@ -61,135 +63,142 @@ def classify(in_dir, out_dir):
 			of.write(outstr)
 	
 
-def resize_image(image_matrix, targ_h, targ_w):
-	h, w, channels = image_matrix.shape
-	new_image_matrix = np.zeros((targ_h, targ_w, channels), dtype='float32')
-	h_scaling_factor = h / targ_h
-	w_scaling_factor = w / targ_w
+def extract_face(image, target_size=(64,64)):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = FACE_CASCADE.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    if len(faces) != 1:
+        return None
+    
+    (x, y, w, h) = faces[0]
+    face = gray[y:y+h, x:x+w]
+    face_resized = cv2.resize(face, target_size)
+    return face_resized
 	
-	for row_ind in range(targ_h):
-		row_nn = row_ind * h_scaling_factor
-		for col_ind in range(targ_w):
-			col_nn = col_ind * w_scaling_factor
-			new_image_matrix[row_ind, col_ind] = image_matrix[row_nn, col_nn]
-	return new_image_matrix
-			
-def read_image(in_dir, user_id):
-	image_path = os.path.join(in_dir, 'image', f'{user_id}.jpg')
-	image = cv2.imread(image_path).astype('float32')
-
-	# Normalize color values before returning
-	return image / 255.0
+def get_base_cnn(input_shape=(64,64,1)):
+	model = models.Sequential([
+	    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1)),
+	    #layers.Dropout(0.1),
+	    layers.Conv2D(32, (3, 3), activation='relu'),
+	    layers.Dropout(0.1),
+	    layers.MaxPooling2D((2, 2)),
 	
+	    layers.Conv2D(64, (3, 3), activation='relu'),
+	    #layers.Dropout(0.1),
+	    layers.Conv2D(64, (3, 3), activation='relu'),
+	    layers.Dropout(0.1),
+	    layers.MaxPooling2D((2, 2)),
+	
+	    layers.Conv2D(64, (3, 3), activation='relu'),
+	    #layers.Dropout(0.1),
+	    layers.Conv2D(64, (3, 3), activation='relu'),
+	    layers.Dropout(0.1),
+	    layers.MaxPooling2D((2, 2)),
+	
+	    layers.Flatten(),
+	    layers.Dense(64, activation='relu')
+	])
+	
+	return model
 
-def train_image_model(in_dir, out_dir):
+def get_faces_df(in_dir):
 	profiles = get_profile_rows(in_dir)
-	genders = [profile['gender'] for profile in profiles]
-	
-	# First, compute min height / width of all images for resizing
-	# purposes. Do not store all images in memory; if we did so,
-	# we'd exhaust RAM.
-	min_height = 999999
-	min_width = 999999
 	images = {
 		"filename": [],
-		"gender": []
+		"gender": [],
+		"age": [],
+		"ope": [],
+		"con": [],
+		"ext": [],
+		"agr": [],
+		"neu": []
 	}
 
-	for profile in profiles:
-		image_matrix = read_image(in_dir, profile['userid'])
-		min_height = min(min_height, image_matrix.shape[0])
-		min_width = min(min_width, image_matrix.shape[1])
+	target_size = (64, 64)
+	face_dir = os.path.join(in_dir, "faces")
+	regen = not os.path.exists(face_dir)
+	if regen:
+		print(f"Regenerating faces in {face_dir}")
+		os.makedirs(face_dir)
 
-		image_path = f"{profile['userid']}.jpg"
-		images["filename"].append(image_path)
-		# Gender is already a scalar value, 0 /1.
-		# (I forgot if 1 is male or female.)
-		images["gender"].append(profile["gender"])
+	for profile in profiles:
+		image_basename = f"{profile['userid']}.jpg"
+		image_path = os.path.join(in_dir, "image", image_basename)
+		face_path = os.path.join(face_dir, image_basename)
+		image_matrix = cv2.imread(image_path)
+
+		face_exists = False
+		if regen:
+			face_matrix = extract_face(image_matrix, target_size)
+			if face_matrix is not None:
+				cv2.imwrite(face_path, face_matrix)
+				face_exists = True
+		else:
+			face_exists = os.path.exists(face_path)
+
+		if face_exists:
+			images["filename"].append(image_basename)
+			images["gender"].append(profile["gender"])
+			images["ope"].append(float(profile["ope"]))
+			images["con"].append(float(profile["con"]))
+			images["ext"].append(float(profile["ext"]))
+			images["agr"].append(float(profile["agr"]))
+			images["neu"].append(float(profile["neu"]))
+
+			age_val = float(profile["age"])
+
+			age = "xx-24"
+			if age_val > 24 and age_val <= 34:
+				age = "25-34"
+			elif age_val > 34 and age_val <= 49:
+				age = "35-49"
+			elif age_val > 49:
+				age = "50-xx"
+
+			images["age"].append(age)
 	
-	images_df = pd.DataFrame(images)
+	faces_df = pd.DataFrame(images)
+	return faces_df
+
+def get_img_gens(face_df, face_dir, attr, class_mode):
+	print(face_df)
 
 	datagen = ImageDataGenerator(rescale=1./255., validation_split=0.1)
 	train_generator = datagen.flow_from_dataframe(
-		dataframe = images_df,
-		directory = os.path.join(in_dir, "image"),
+		dataframe = face_df,
+		directory = face_dir,
 		x_col="filename",
-		y_col="gender",
+		y_col=attr,
 		subset="training",
 		batch_size=32,
 		seed=42,
 		shuffle=True,
-		class_mode="binary",
-		target_size=(96, 96)
+		class_mode=class_mode,
+		target_size=(64,64),
+		color_mode="grayscale"
 	)
 
 	validation_generator = datagen.flow_from_dataframe(
-		dataframe = images_df,
-		directory = os.path.join(in_dir, "image"),
+		dataframe = face_df,
+		directory = face_dir,
 		x_col="filename",
-		y_col="gender",
+		y_col=attr,
 		subset="validation",
 		batch_size=32,
 		seed=42,
 		shuffle=True,
-		class_mode="binary",
-		target_size=(96, 96)
+		class_mode=class_mode,
+		target_size=(64,64),
+		color_mode="grayscale"
+
 	)
 
-	# The model structure is taken from https://github.com/oarriaga/face_classification
-	# (code accompanying Arriaga et al [2017]), except I add some fully connected 
-	# layers at the end.
-	model = Sequential()
-	model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same',
-	                        name='image_array', input_shape=(96,96,3)))
-	model.add(BatchNormalization())
-	model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-	model.add(Dropout(.5))
-	
-	model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-	model.add(Dropout(.5))
-	
-	model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-	model.add(Dropout(.5))
-	
-	model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Activation('relu'))
-	model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-	model.add(Dropout(.5))
-	
-	model.add(Convolution2D(filters=256, kernel_size=(3, 3), padding='same'))
-	model.add(BatchNormalization())
-	model.add(Convolution2D(
-	    filters=1, kernel_size=(3, 3), padding='same'))
-	model.add(GlobalAveragePooling2D())
-	model.add(Dense(32))
-	model.add(Dropout(0.5))
-	model.add(Dense(1, activation='sigmoid'))
-	print(f"Model summary: {model.summary()}")
+	return (train_generator, validation_generator)
 
-	model.compile(optimizer='adam', loss='binary_crossentropy', 
-		metrics=['accuracy'])
-	
-	step_size_train = train_generator.n // train_generator.batch_size
-	step_size_validate = validation_generator.n // validation_generator.batch_size
+def train_model(model, train_gen, val_gen, checkpt_path):
+	step_size_train = train_gen.n // train_gen.batch_size
+	step_size_validate = val_gen.n // val_gen.batch_size
 
-	checkpt_path = os.path.join(out_dir, 'model_checkpoint.h5')
 	checkpoint = keras.callbacks.ModelCheckpoint(
 		checkpt_path,
 		monitor='val_loss',
@@ -205,24 +214,84 @@ def train_image_model(in_dir, out_dir):
     	restore_best_weights=True
 	)
 
-
 	history = model.fit(
-		train_generator,
+		train_gen,
 		steps_per_epoch=step_size_train,
-		validation_data=validation_generator,
+		validation_data=val_gen,
 		validation_steps=step_size_validate,
 		epochs=50,
 		callbacks=[checkpoint, early_stopping]
 	)
 
 	val_loss, val_acc = model.evaluate(
-		validation_generator,
+		val_gen,
 		steps=step_size_validate
 	)
 
-	print(f"Achieved val loss {val_loss}, val acc {val_acc}")
-	return (val_loss, val_acc, history)
-	
+	return (val_loss, val_acc)
+
+
+def train_image_models(in_dir, out_dir):
+	face_dir = os.path.join(in_dir, "faces")
+	print(f"Face dir {face_dir}")
+	face_df = get_faces_df(in_dir)
+	(gender_train_gen, gender_val_gen) = get_img_gens(
+		face_df, 
+		face_dir, 
+		"gender", 
+		"binary"
+	)
+
+	# Base CNN contains contolutional / pooling / FC layers.
+	# Now we just add 1-node sigmoid layer for binary classification.
+	# gender_checkpt_path = os.path.join(out_dir, "gender.h5")
+	# gender_model = get_base_cnn()
+	# gender_model.add(Dropout(0.5))
+	# gender_model.add(Dense(1, activation='sigmoid'))
+	# gender_model.compile(
+	# 	optimizer='adam', 
+	# 	loss='binary_crossentropy', 
+	# 	metrics=['accuracy']
+	# )
+	# gender_loss, gender_acc = train_model(
+	# 	gender_model, 
+	# 	gender_train_gen, 
+	# 	gender_val_gen,
+	# 	gender_checkpt_path
+	# )
+
+	# print(f"Gender model summary:")
+	# gender_model.summary()
+	# print(f"Achieved {gender_loss} loss and {gender_acc} accuracy on 10%"
+	#	f"validation set for gender model.")
+
+	# This screws up and only gives one path
+	(age_train_gen, age_val_gen) = get_img_gens(
+		face_df, 
+		face_dir, 
+		"age",
+		"categorical"
+	)
+	age_checkpt_path = os.path.join(out_dir, "age.h5")
+	age_model = get_base_cnn()
+	age_model.add(Dense(16, activation='relu'))
+	age_model.add(Dense(4, activation='softmax'))
+	age_model.compile(
+		optimizer='adam', 
+		loss='categorical_crossentropy', 
+		metrics=['accuracy']
+	)
+	age_loss, age_acc = train_model(
+		age_model, 
+		age_train_gen, 
+		age_val_gen, 
+		age_checkpt_path
+	)
+
+	print(f"Age model summary:")
+	age_model.summary()
+	print(f"Achieved {age_loss} loss and {age_acc} accuracy on 10%"
+		f"validation set for age model.")
 			
 	
 
@@ -232,13 +301,13 @@ def train_text_model(in_dir, out_dir):
 def train_like_model(in_dir, out_dir):
 	pass
 
-def train_model(in_dir, out_dir, modality):
+def train_models(in_dir, out_dir, modality):
 	if modality == "image":
-		return train_image_model(in_dir, out_dir)
+		train_image_models(in_dir, out_dir)
 	elif modality == "text":
-		return train_text_model(in_dir, out_dir)
+		train_text_model(in_dir, out_dir)
 	elif modality == "like":
-		return train_like_model(in_dir, out_dir)
+		train_like_model(in_dir, out_dir)
 	
 	print(f"Error: {modality} is not 'image', 'text', or 'like'")
 	exit(1)
@@ -259,7 +328,7 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	if args.train:
 		print(f"Training {args.train} model")
-		train_model(args.input_dir, args.output_dir, args.train)
+		train_models(args.input_dir, args.output_dir, args.train)
 
 	else:
 		print(f"Classifying")
